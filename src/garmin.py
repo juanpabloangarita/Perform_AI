00bd841360848af540b3c5ad74f9b43ddcc160ed
juanpablo angarita <juanpabloangarita@gmail.com>
2024-08-31 22:15:59 +0200

    still being detected

--- src/garmin.py
+++ src/garmin.py
@@ -38,36 +38,26 @@ def setup_driver(options, is_simple=True):
         driver = webdriver.Chrome(service=Service(chrome_driver_path, log_path='chromedriver.log'), options=options)
     return driver
 
+# Function to add cookies to the driver
+def add_cookies(driver, cookies):
+    for cookie in cookies:
+        driver.add_cookie(cookie)
 
-def navigate_to_login_simple():
-    # Create Chrome options instance
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-    # options.add_argument("--headless")  # Uncomment this line to run in headless mode
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("--disable-search-engine-choice-screen")
-
-    # Create a temporary user data directory
-    temp_user_data_dir = tempfile.mkdtemp()
-    options.add_argument(f"--user-data-dir={temp_user_data_dir}")
-
-    # Set the path to Chrome binary explicitly
-    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
-    # Set up the ChromeDriver with the specified Chrome binary
-    driver = setup_driver(options)
-
-    # Open the Garmin Connect login page
-    driver.get("https://sso.garmin.com/portal/sso/en-GB/sign-in?clientId=GarminConnect&service=https%3A%2F%2Fconnect.garmin.com%2Fmodern")
-
-    print(driver.title)
-    input()
-    driver.quit()
+# Load cookies into the driver
+def load_cookies():
+    # Replace this with actual values you have extracted
+    cookies = [
+        {"name": "SESSION", "value": "NzQ2YmI0MWItY2QzMC00MmQ0LWJiYzUtNzJmMDkzOTliY2Zh", "domain": "sso.garmin.com"},
+        {"name": "__VCAP_ID__", "value": "47d0f134-e4b5-4348-7807-d782", "domain": "sso.garmin.com"},
+        {"name": "cf_clearance", "value": "St2C9FjcQSCYsMS8yxJ3ZKfdvuIfHnHJZpcQ.hHY8gs-1723210272-1.0.1.1-z89QpiyE9u.2KASTfIxFMJ9uJ58PCUZ8mifePZ2MUpsvK2xeGIa40jQwPO2RHkG2XfjvpqT_tzgHPt9AfY8v0A", "domain": "sso.garmin.com"},
+        {"name": "SameSite", "value": "None", "domain": "sso.garmin.com"},
+        {"name": "__cflb", "value": "02DiuEofTPbaQ2tyBEVR6YcR5RQYPgpFY26dLh9KigZtG", "domain": "sso.garmin.com"},
+        {"name": "__cf_bm", "value": "GvAmHBsN6UKnmabksl3eonhOef23gO8ucIpQ50V3zdo-1724868769-1.0.1.1-VUTNws1QwSXik1DcKdwvh1qo9yJLt2x1oT314pAROQOs5I3GifTNqcBAJ1WSSEjGq15CGWE4wu3UeUagSS9OvA", "domain": "sso.garmin.com"},
+        {"name": "_cfuvid", "value": "Rc6d2uCACOfnGUwigboqXJcE8QUOkbBYPrGbEdhF7to-1724868769034-0.0.1.1-604800000", "domain": "sso.garmin.com"},
+        {"name": "ADRUM_BTa", "value": "R:0|g:3a47900d-1754-4253-b2af-a6923b852afd", "domain": "sso.garmin.com"},
+        {"name": "ADRUM_BT1", "value": "R:0|i:2270382|e:120|t:1724868781116", "domain": "sso.garmin.com"}
+    ]
+    return cookies
 
 
 def navigate_to_login_advanced():
@@ -95,6 +85,7 @@ def navigate_to_login_advanced():
     # How to get rid of "Choose your search engine" dialog in Chrome v.127
     options.add_argument("--disable-search-engine-choice-screen") # https://stackoverflow.com/questions/78798750/how-to-get-rid-of-choose-your-search-engine-dialog-in-chrome-v-127-on-selenium
     # options.add_argument("--headless")  # Run headless, remove this if you want to see the browser
+    # options.add_argument("--auto-open-devtools-for-tabs") #not sure for the moment
 
     # Create a temporary directory for the user data
     temp_user_data_dir = tempfile.mkdtemp()
@@ -106,8 +97,8 @@ def navigate_to_login_advanced():
     # Set up a user profile directory (optional, prevents saving of any profile info)
     options.add_argument("--user-data-dir=/tmp/temporary-chrome-profile")  # Use a temporary profile
 
-    # Setting user-agent
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+    # Setting user-agent to the latest version from inspection
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36" #
     options.add_argument(f"user-agent={user_agent}")
 
     # Initialize WebDriver with Proxy
@@ -124,20 +115,37 @@ def navigate_to_login_advanced():
         run_on_insecure_origins=True
     )
 
+    # Add this new script execution below: ### NEW
+    driver.execute_script("""
+        Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
+        Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
+        Object.defineProperty(navigator, 'languages', {get: () => ['en-GB', 'en']});
+        window.screen = {width: 1920, height: 1080, availWidth: 1920, availHeight: 1080};
+        window.outerWidth = window.innerWidth;
+        window.outerHeight = window.innerHeight;
+    """)
+
     # Changing the property of the navigator value for webdriver to undefined
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    # driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") #
 
     # Custom headers
     custom_headers = {
         "User-Agent": user_agent,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
+        # "Accept-Encoding": "gzip, deflate, br",
+        # "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6,es;q=0.5",
+        "Cache-Control": "max-age=0", #
+        "Sec-Fetch-Dest": "document", #
+        "Sec-Fetch-Mode": "navigate", #
+        # "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1", #
         "Upgrade-Insecure-Requests": "1",
+        "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"", #
+        "sec-ch-ua-mobile": "?0", #
+        "sec-ch-ua-platform": "\"macOS\"", #
         "Referer": "https://www.garmin.com"
     }
     # Set additional common headers to mimic a typical browser session
@@ -161,35 +169,30 @@ def navigate_to_login_advanced():
     # Set the custom headers
     set_custom_headers(driver, all_headers)
 
+    # Mimic network conditions for real-world scenario ### NEW
+    driver.execute_cdp_cmd('Network.emulateNetworkConditions', {
+        'offline': False,
+        'latency': 100,  # 100ms latency
+        'downloadThroughput': 500 * 1024,  # 500 kb/s
+        'uploadThroughput': 500 * 1024,  # 500 kb/s
+    })
+
+    # Set cookies before navigating
+    cookies = load_cookies()
     # Open the Garmin Connect login page
     driver.get("https://sso.garmin.com/portal/sso/en-GB/sign-in?clientId=GarminConnect&service=https%3A%2F%2Fconnect.garmin.com%2Fmodern")
-
-    # time.sleep(3)  # Wait for the page to load
-
+    # add cookies
+    #add_cookies(driver, cookies)
+    #time.sleep(3)  # Wait for the page to load
+    # Refresh to apply cookies
+    #driver.refresh()
     # # Wait for the page to load and set cookies
-    # time.sleep(3)
+    #time.sleep(3)
     # Keep the browser window open
     print(driver.title)
     input()
     driver.quit()
 
-
-# Uncomment one of the functions below to test
-# navigate_to_login_simple()
-navigate_to_login_advanced()
-
-
-# # Extract and set cookies from a prior session or response
-# cookies_header = "SESSION=NDQ5ZjE0MWEtMTU4ZS00YjUyLTlmNWYtNzM5ODgxMGFkMGEz; __VCAP_ID__=085b90dd-b842-40c2-7c5d-b12d; GarminUserPrefs=en-GB; __cflb=02DiuEofTPbaQ2tyBEVR6YcR5RQYPgpFZ1erLMDvdbay2; _cfuvid=ggLto8vv5yULggbEIcGQCJ5NI8mg8CYIzClkFJPsFKQ-1724605422525-0.0.1.1-604800000; cf_clearance=yhQqEnVG34w15p_3yO_YezG83zwF8vfJNnxGDMwwS9Q-1724672483-1.2.1.1-oYySv2jl4jC_LHsSDusSQKTAfEncKv_zwEjlW.Iq4mlHOJV1ED0BScCdO5qON4LvVgmZ3zpVCm8At6T4e7yXurhWqNJzx8ITK_vBvS3Mnpov._GFgki30NzRnlLuuYQLkFDFnkEjjwh6wUT_GkTvmr574M7V2copwUPDRCajuBIExfOy6y9oDq8FmGD20Jw5KjMDNTXfYmY5tXuClzOU1wGkxzrj_SnDUzlzfUAQFFqcGY9BrXgvy4PpQ6WZl_BmpN.chfFhDx6pspkeqZhNBuuJcmoK4KiZJEaLE.ZJlBo6ledBQVgbxs1hlsMGbQko1hbuWXuIIxGUzaRy6l61vxpYHopI6a7zBf1bnxiLBAa4V_2cinc4sBwObd0epBECf8YeRdgnQTO5S2iVxTrrIw"
-# for cookie in cookies_header.split('; '):
-#     if '=' in cookie:
-#         name, value = cookie.split('=', 1)
-#         driver.add_cookie({'name': name, 'value': value, 'domain': '.garmin.com'})
-#     else:
-#         print(f"Skipping invalid cookie: {cookie}")
-
-# # Refresh the page to apply cookies and custom headers
-# driver.refresh()
 
 # # Wait and continue with additional actions
 # time.sleep(5)
@@ -238,3 +241,39 @@ navigate_to_login_advanced()
 #     print(f"An error occurred: {e}")
 
 # ##||
+
+
+def navigate_to_login_simple():
+    # Create Chrome options instance
+    options = Options()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    # options.add_argument("--headless")  # Uncomment this line to run in headless mode
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-search-engine-choice-screen")
+
+    # Create a temporary user data directory
+    temp_user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f"--user-data-dir={temp_user_data_dir}")
+
+    # Set the path to Chrome binary explicitly
+    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+
+    # Set up the ChromeDriver with the specified Chrome binary
+    driver = setup_driver(options)
+
+    # Open the Garmin Connect login page
+    driver.get("https://sso.garmin.com/portal/sso/en-GB/sign-in?clientId=GarminConnect&service=https%3A%2F%2Fconnect.garmin.com%2Fmodern")
+
+    print(driver.title)
+    input()
+    driver.quit()
+
+
+# Uncomment one of the functions below to test
+# navigate_to_login_simple()
+navigate_to_login_advanced()
