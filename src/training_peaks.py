17b04ee5cdc6607607e1722c4f2a7d365c64804b
juanpablo angarita <juanpabloangarita@gmail.com>
2024-09-25 11:05:17 +0200

    To modify to adapt for streamlit cloud

--- src/training_peaks.py
+++ src/training_peaks.py
@@ -19,16 +19,36 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from params import *
 
+headless_mode = (CLOUD_ON == 'yes')
+
 # Set environment variable for browser (for debugging purposes)
 os.environ["BROWSER"] = "chrome"
 os.environ['WDM_SKIP_VERSION_CHECK'] = 'true'
 
 def setup_driver(options):
+    # Add headless mode options if needed
+    if headless_mode:
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("window-size=1920,1080")
+    options.add_argument("--disable-web-security")  # Disables web security
+    temp_user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f"--user-data-dir={temp_user_data_dir}")
+
+
+    # Add options to prevent bot detection
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+
     # Set up the ChromeDriver
     chrome_driver_path = ChromeDriverManager().install()
     service = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=options)
     return driver
+
 
 # Function to add cookies to the driver
 def add_cookies(driver, cookies):
