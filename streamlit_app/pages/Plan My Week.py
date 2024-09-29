2e4a0224202c6937f60e86d88852a06163df1baf
juanpablo angarita <juanpabloangarita@gmail.com>
2024-09-29 11:44:13 +0200

    corrected plan my week with proiper instant IP and not ID on one part, updated params for INSTANCE IP

--- streamlit_app/pages/Plan My Week.py
+++ streamlit_app/pages/Plan My Week.py
@@ -29,7 +29,7 @@ def training_peaks_button_helper():
 
     # Step 2: Run the training_peaks.py script on EC2 via SSM
     # command_status, exit_code = run_script_on_ec2(INSTANCE_ID)
-    command_status = run_script_via_ssh(INSTANCE_ID)
+    command_status = run_script_via_ssh(INSTANCE_IP)
 
     # Step 3: Stop the EC2 instance to save costs
     stop_ec2_instance(INSTANCE_ID)
